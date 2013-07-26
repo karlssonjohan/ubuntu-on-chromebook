b2f215c283f0cb0c5c2ed1a58d2159b7b2bab02e
Johan <jmakarlsson@yahoo.com>
2013-07-26 16:16:55 +0200

    fix

--- post-install.sh
+++ post-install.sh
@@ -5,4 +5,4 @@ source /usr/local/etc/uoc-config
 mkdir $MP/etc/X11/xorg.conf.d
 cp ubuntu/x.conf $MP/etc/X11/xorg.conf.d
 chroot $MP bash -c "init-chroot-env.sh;uoc-install.sh"
-sudo bash ./ubuntu/setupkernel.sh"
+sudo bash ./ubuntu/setupkernel.sh
